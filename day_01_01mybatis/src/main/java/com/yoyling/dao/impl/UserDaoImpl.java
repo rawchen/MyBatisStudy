3fac04eabf9f7b774b9f186a6f016e4c980303c5
YOYLING <2221999792@qq.com>
2020-08-21 22:49:10 +0800

    Add files via upload

---
+++ day_01_01mybatis/src/main/java/com/yoyling/dao/impl/UserDaoImpl.java
@@ -0,0 +1,12 @@
+package com.yoyling.dao.impl;
+
+import com.yoyling.dao.UserDao;
+import com.yoyling.domain.User;
+
+import java.util.List;
+
+public class UserDaoImpl implements UserDao {
+    public List<User> findAll() {
+        return null;
+    }
+}
