01b99a7c6df4370ee61e4a7f337c5a122dcf920a
YOYLING <2221999792@qq.com>
2020-08-23 22:20:35 +0800

    Add files via upload

---
+++ day_01_01mybatis/src/test/java/com/yoyling/test/MyBatisTest.java
@@ -0,0 +1,14 @@
+package com.yoyling.test;
+
+/**
+ * mybatis的入门案例
+ */
+public class MyBatisTest {
+    /**
+     * 入门案例
+     * @param args
+     */
+    public static void main(String[] args) {
+        //1.读取配置文件
+    }
+}
