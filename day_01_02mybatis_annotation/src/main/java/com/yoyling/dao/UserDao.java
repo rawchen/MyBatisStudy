7c3e1286b66b0b24fefd061adec38599857e3142
YOYLING <2221999792@qq.com>
2020-08-25 23:59:34 +0800

    Add files via upload

---
+++ day_01_02mybatis_annotation/src/main/java/com/yoyling/dao/UserDao.java
@@ -0,0 +1,17 @@
+package com.yoyling.dao;
+
+import com.yoyling.domain.User;
+
+import java.util.List;
+
+/**
+ * 用户的持久层接口
+ */
+public interface UserDao {
+
+    /**
+     * 查询所有操作
+     * @return
+     */
+    List<User> findAll();
+}
