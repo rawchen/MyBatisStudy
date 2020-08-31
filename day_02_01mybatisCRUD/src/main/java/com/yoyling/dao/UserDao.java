4cb4571557fe8acfca4bc73d19d35e7baeca6f1b
YOYLING <2221999792@qq.com>
2020-08-31 23:48:17 +0800

    Add files via upload

--- day_02_01mybatisCRUD/src/main/java/com/yoyling/dao/UserDao.java
+++ day_02_01mybatisCRUD/src/main/java/com/yoyling/dao/UserDao.java
@@ -14,4 +14,30 @@ public interface UserDao {
      * @return
      */
     List<User> findAll();
+
+    /**
+     * 保存用户
+     * @param user
+     */
+    void saveUser(User user);
+
+    /**
+     * 更新用户
+     * @param user
+     */
+    void updateUser(User user);
+
+    /**
+     * 根据id删除用户
+     * @param userId
+     */
+    void deleteUser(Integer userId);
+
+    /**
+     * 根据id查询用户信息
+     * @param userId
+     * @return
+     */
+    User findById(Integer userId);
+
 }
--- day_02_01mybatisCRUD/src/test/java/com/yoyling/test/MybatisTest.java
+++ day_02_01mybatisCRUD/src/test/java/com/yoyling/test/MybatisTest.java
@@ -6,9 +6,12 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.InputStream;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -16,17 +19,78 @@ import java.util.List;
  */
 public class MybatisTest {
 
-    @Test
-    public void testFindAll() throws Exception {
-        InputStream in = Resources.getResourceAsStream("sqlMapConfig.xml");
+    private InputStream in;
+    private SqlSession sqlSession;
+    private UserDao userDao;
+
+    @Before     //用于在测试方法执行之前执行
+    public void init() throws Exception {
+        in = Resources.getResourceAsStream("sqlMapConfig.xml");
         SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
-        SqlSession sqlSession = factory.openSession();
-        UserDao userDao = sqlSession.getMapper(UserDao.class);
+        sqlSession = factory.openSession();
+        userDao = sqlSession.getMapper(UserDao.class);
+    }
+
+    @After     //用于在测试方法执行之后执行
+    public void destroy() throws Exception {
+        //提交事务
+        sqlSession.commit();
+
+        sqlSession.close();
+        in.close();
+    }
+
+    @Test
+    public void testFindAll(){
         List<User> users = userDao.findAll();
         for (User user : users) {
             System.out.println(user);
         }
-        sqlSession.close();
-        in.close();
+    }
+
+    /**
+     * 测试保存操作
+     */
+    @Test
+    public void testSave() {
+        User user = new User();
+        user.setUsername("yoyling");
+        user.setAddress("福建省厦门市");
+        user.setSex("男");
+        user.setBirthday(new Date());
+
+        userDao.saveUser(user);
+    }
+
+    /**
+     * 测试更新操作
+     */
+    @Test
+    public void testUpdate() {
+        User user = new User();
+        user.setId(50);
+        user.setUsername("mybatis");
+        user.setAddress("福建省漳州市");
+        user.setSex("女");
+        user.setBirthday(new Date());
+
+        userDao.updateUser(user);
+    }
+
+    /**
+     * 测试删除操作
+     */
+    @Test
+    public void testDelete() {
+        userDao.deleteUser(50);
+    }
+
+    /**
+     * 测试查询一个操作
+     */
+    @Test
+    public void testFindOne() {
+        User user = userDao.findById(48);
+        System.out.println(user);
     }
 }
