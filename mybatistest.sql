4e54b5abb3dd32a2d546c11a45c0528cbc0113a7
YOYLING <2221999792@qq.com>
2020-10-07 23:56:29 +0800

    Add files via upload

--- mybatistest.sql
+++ mybatistest.sql
@@ -12,8 +12,51 @@ CREATE TABLE `user` (
 insert  into `user`(`id`,`username`,`birthday`,`sex`,`address`) 
 values 
 (41,'老王','2018-02-27 17:47:08','男','北京'),
-(42,'小二王','2018-03-02 15:09:37','女','北京金燕龙'),
-(43,'小二王','2018-03-04 11:34:34','女','北京金燕龙'),
-(45,'传智播客','2018-03-04 12:04:06','男','北京金燕龙'),
-(46,'老王','2018-03-07 17:37:26','男','北京'),
-(48,'小马宝莉','2018-03-08 11:44:00','女','北京修正');
+(42,'小二王','2018-03-02 15:09:37','女','福建'),
+(43,'小二王','2018-03-04 11:34:34','女','厦门'),
+(45,'老六','2018-03-04 12:04:06','男','西藏'),
+(46,'老王','2018-03-07 17:37:26','男','新疆'),
+(48,'小马宝莉','2018-03-08 11:44:00','女','湖南');
+
+
+
+DROP TABLE IF EXISTS `account`;
+
+CREATE TABLE `account` (
+  `ID` int(11) NOT NULL COMMENT '编号',
+  `UID` int(11) default NULL COMMENT '用户编号',
+  `MONEY` double default NULL COMMENT '金额',
+  PRIMARY KEY  (`ID`),
+  KEY `FK_Reference_8` (`UID`),
+  CONSTRAINT `FK_Reference_8` FOREIGN KEY (`UID`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+insert  into `account`(`ID`,`UID`,`MONEY`) values (1,46,1000),(2,45,1000),(3,46,2000);
+
+
+
+DROP TABLE IF EXISTS `role`;
+
+CREATE TABLE `role` (
+  `ID` int(11) NOT NULL COMMENT '编号',
+  `ROLE_NAME` varchar(30) default NULL COMMENT '角色名称',
+  `ROLE_DESC` varchar(60) default NULL COMMENT '角色描述',
+  PRIMARY KEY  (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+insert  into `role`(`ID`,`ROLE_NAME`,`ROLE_DESC`) values (1,'院长','管理整个学院'),(2,'总裁','管理整个公司'),(3,'校长','管理整个学校');
+
+
+
+DROP TABLE IF EXISTS `user_role`;
+
+CREATE TABLE `user_role` (
+  `UID` int(11) NOT NULL COMMENT '用户编号',
+  `RID` int(11) NOT NULL COMMENT '角色编号',
+  PRIMARY KEY  (`UID`,`RID`),
+  KEY `FK_Reference_10` (`RID`),
+  CONSTRAINT `FK_Reference_10` FOREIGN KEY (`RID`) REFERENCES `role` (`ID`),
+  CONSTRAINT `FK_Reference_9` FOREIGN KEY (`UID`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+insert  into `user_role`(`UID`,`RID`) values (41,1),(45,1),(41,2);
