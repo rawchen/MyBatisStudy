35c97226bea1359c5d2b9370c30b38bc17a8d23f
YOYLING <2221999792@qq.com>
2020-10-08 00:03:49 +0800

    Add files via upload

---
+++ day_03_04many2many/src/main/java/com/yoyling/domain/Role.java
@@ -0,0 +1,7 @@
+package com.yoyling.domain;
+
+import java.io.Serializable;
+
+public class Role implements Serializable {
+
+}
---
+++ day_03_04many2many/src/test/java/com/yoyling/test/UserTest.java
@@ -0,0 +1,45 @@
+package com.yoyling.test;
+
+import com.yoyling.dao.UserDao;
+import com.yoyling.domain.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.InputStream;
+import java.util.List;
+
+public class UserTest {
+    private InputStream in;
+    private SqlSession sqlSession;
+    private UserDao userDao;
+
+    @Before     //用于在测试方法执行之前执行
+    public void init() throws Exception {
+        in = Resources.getResourceAsStream("sqlMapConfig.xml");
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
+        sqlSession = factory.openSession(true);
+        userDao = sqlSession.getMapper(UserDao.class);
+    }
+
+    @After     //用于在测试方法执行之后执行
+    public void destroy() throws Exception {
+        //提交事务
+        //sqlSession.commit();
+
+        sqlSession.close();
+        in.close();
+    }
+
+    @Test
+    public void testFindAll(){
+        List<User> users = userDao.findAll();
+        for (User user : users) {
+            System.out.println(user);
+        }
+    }
+}
