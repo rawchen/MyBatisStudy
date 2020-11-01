6fbc3fdbb95a6660a58336b1232a2163feff0557
YOYLING <2221999792@qq.com>
2020-11-01 17:17:56 +0800

    Add files via upload

---
+++ day_04_01lazy/src/main/java/com/yoyling/dao/AccountDao.java
@@ -0,0 +1,21 @@
+package com.yoyling.dao;
+
+import com.yoyling.domain.Account;
+
+import java.util.List;
+
+public interface AccountDao {
+
+    /**
+     * 查询所有账户，同时还要获取到当前账户的所属用户信息
+     * @return
+     */
+    List<Account> findAll();
+
+    /**
+     * 根据用户id查询账户信息
+     * @param uid
+     * @return
+     */
+    List<Account> findAccountByUid(Integer uid);
+}
---
+++ day_04_01lazy/src/test/java/com/yoyling/test/AccountTest.java
@@ -0,0 +1,46 @@
+package com.yoyling.test;
+
+import com.yoyling.dao.AccountDao;
+import com.yoyling.domain.Account;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.InputStream;
+import java.util.List;
+
+public class AccountTest {
+    private InputStream in;
+    private SqlSession sqlSession;
+    private AccountDao accountDao;
+
+    @Before     //用于在测试方法执行之前执行
+    public void init() throws Exception {
+        in = Resources.getResourceAsStream("sqlMapConfig.xml");
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
+        sqlSession = factory.openSession(true);
+        accountDao = sqlSession.getMapper(AccountDao.class);
+    }
+
+    @After     //用于在测试方法执行之后执行
+    public void destroy() throws Exception {
+        //提交事务
+        //sqlSession.commit();
+
+        sqlSession.close();
+        in.close();
+    }
+
+    @Test
+    public void testFindAll(){
+        List<Account> users = accountDao.findAll();
+//        for (Account account : users) {
+//            System.out.println(account);
+//            System.out.println(account.getUser());
+//        }
+    }
+}
